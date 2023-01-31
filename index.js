69f7fded9dd9ff56038f3583925f3564a2d622f6
Chandira Anupama <chandira.anupamawca@gmail.com>
2023-01-31 22:19:39 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const axios = require('axios');
+const firebase = require('firebase-admin');
+const cron = require('node-cron');
+
+const app = express();
+const botToken = '5977867332:AAFz8bGw2pTuGZlgwYMaFA2UKAO451dL6pY';
+const chatId = '-1001823334104';
+var port = process.env.PORT || 5000;
+
+// Initialize Firebase
+const serviceAccount = require('./serviceAccountKey.json');
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://english-re-edu-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
+const database = firebase.database();
+
+// Schedule task to run 6 times a day
+cron.schedule('0 */2 * * *', () => {
+  database.ref().once('value', snapshot => {
+    const data = snapshot.val();
+    var datatosend = JSON.stringify(data);
+
+    // Send data to Telegram channel
+    axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+      chat_id: chatId,
+      text: "✅ Student Database Automatic Backup Successfull\n\n"+ datatosend
+    })
+      .then(() => {
+        console.log('Data sent to Telegram channel');
+      })
+      .catch(error => {
+        console.error(error);
+        console.log('Error sending data to Telegram channel (Automatic)');
+      });
+  });
+});
+
+app.get('/', (req, res) => {
+    database.ref().once('value', snapshot => {
+      const data = snapshot.val();
+      var datatosend = JSON.stringify(data);
+  
+      // Send data to Telegram channel
+      axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+        chat_id: chatId,
+        text: "✅ Student Database Manual Backup Successfull\n\n"+ datatosend
+      })
+        .then(() => {
+          console.log('Data sent to Telegram channel (Manual)');
+          res.send('Data sent to Telegram channel');
+        })
+        .catch(error => {
+          console.error(error);
+          console.log('Error sending data to Telegram channel');
+          res.send('Error sending data to Telegram channel');
+        });
+    });
+  });
+
+  app.listen(port);
+  console.log('Server started! At http://localhost:' + port);
